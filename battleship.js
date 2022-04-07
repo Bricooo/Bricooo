9eb7582d4c45546037175fe3242552e9087c3fd1
Bricooo <98779206+Bricooo@users.noreply.github.com>
2022-04-07 18:50:32 +0200

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -12,7 +12,17 @@ while (isSunk = false) {
     } else {
         guesses = guesses + 1;
         if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
             hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS");
+            }
         }
     }
 }
+var stats = "You took " + guesses + " guesses to sink the battleship. " + "wich means your shooting accuracy was " + (3/guesses);
+alert(stats);
